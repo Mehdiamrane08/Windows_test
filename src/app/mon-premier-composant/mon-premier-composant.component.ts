a02bdb9f9aed30dd934e3dcaaaa238b21f26e610
Mehdi <mehdiamranepro@gmail.com>
2023-10-25 09:41:56 +0200

    firs commit

---
+++ src/app/mon-premier-composant/mon-premier-composant.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mon-premier-composant',
+  templateUrl: './mon-premier-composant.component.html',
+  styleUrls: ['./mon-premier-composant.component.css']
+})
+export class MonPremierComposantComponent {
+
+}
